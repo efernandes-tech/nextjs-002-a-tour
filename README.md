f7d13dda7692877a06a34835014e649187eb929f
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-17 12:29:35 -0300

    Estrutura de projeto!

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # nextjs-002-a-tour
+
+## Run:
+
+```
+cd aula01/a1.3
+npm install
+npm run dev
+```
---
+++ aula01/a1.3/pages/sobre.js
@@ -0,0 +1,9 @@
+function SobrePage() {
+  return (
+    <div>
+      Vc está na página sobre!
+    </div>
+  )
+}
+
+export default SobrePage
