7ba8726be253232dcb0483c1bab4b6a5b98d34d3
Éderson Fernandes <ederson.fernandes@bempromotora.onmicrosoft.com>
2022-09-19 17:05:46 -0300

    Tipagens do Next.js

--- aula03/a3-typescript/pages/index.tsx
+++ aula03/a3-typescript/pages/index.tsx
@@ -1,3 +1,12 @@
+import type { GetStaticProps } from "next";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  return {
+    props: {},
+    revalidate: 60
+  };
+};
+
 export default function Home() {
   return (
     <div>
--- aula03/a3-typescript/pages/sobre.tsx
+++ aula03/a3-typescript/pages/sobre.tsx
@@ -1,3 +1,11 @@
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {},
+  };
+};
+
 export default function Home() {
   return (
     <div>
